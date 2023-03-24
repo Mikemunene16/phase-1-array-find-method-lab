b20a276c32c04e10e1a45174d702678930dbb86b
Mike <mike.munene@student.moringaschool.com>
2023-03-24 16:40:58 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 // code your solution here
 function superbowlWin(arr) {
-    for (const { result, year } of arr) {
-        if (result === "W") {
-            return year;
-        }
-    }
+    // for (const { result, year } of arr) {
+    //     if (result === "W") {
+    //         return year;
+    //     }
+    // }
+
+    const win = arr.find((obj) => obj.result === "W");
+    return win ? win.year : undefined;
 }
